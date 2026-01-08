37195383262817db67fa741e48b00293dc479fc2
Gayathridevi-123 <gayubhuva53@gmail.com>
2026-01-08 22:22:44 +0530

    updated projects

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -30,7 +30,7 @@ function Checkout() {
     console.log("CART ITEMS 👉", cart); // ✅ products print
 
     try {
-      const res = await fetch("${import.meta.env.VITE_API_URL}/api/orders", {
+      const res = await fetch("http://localhost:5000/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
